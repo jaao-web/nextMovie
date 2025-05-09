82d9aaeddc468bf34647bca001a0064e139827cd
João Vitor Garcia Andrade <80601634+jaao-web@users.noreply.github.com>
2025-05-09 15:23:11 -0300

    spring init

---
+++ src/main/java/com/nextmovie/nextMovie/NextMovieApplication.java
@@ -0,0 +1,13 @@
+package com.nextmovie.nextMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextMovieApplication.class, args);
+	}
+
+}
