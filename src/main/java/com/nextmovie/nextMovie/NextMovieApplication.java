ce9b4164c528f88079a92d57aa89bba51813e15b
João Vitor Garcia Andrade <80601634+jaao-web@users.noreply.github.com>
2025-05-09 15:30:11 -0300

    (exclude = {DataSourceAutoConfiguration.class }) nao sei o que isso fazia?

--- src/main/java/com/nextmovie/nextMovie/NextMovieApplication.java
+++ src/main/java/com/nextmovie/nextMovie/NextMovieApplication.java
@@ -2,8 +2,9 @@ package com.nextmovie.nextMovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class NextMovieApplication {
 
 	public static void main(String[] args) {
